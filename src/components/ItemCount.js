a194ba3123ad646503ac0c383d190e976ac5f44c
Emiliano Bonalumi <emibona@yahoo.com.ar>
2021-08-04 11:48:51 -0300

    se agrega contador de productos

---
+++ src/components/ItemCount.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Container, Card, Row, Col, Form, Button } from "react-bootstrap";
+import Swal from 'sweetalert2';
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setCount] = useState(parseInt(initial));
+  const [disponible, setDisponible] = useState(parseInt(stock));
+  const [producto, setProducto] = useState('AXE Marine 190 ml');
+
+  const incrementar = () => {
+    if (count < disponible) {
+      setCount(count + 1);
+    } else {
+      Swal.fire({
+        title: 'Disponible',
+        text: 'Ha superado la cantidad disponible',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      })
+    }
+  };
+
+  const decrementar = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: "30%" }}>
+              <Card.Body>
+                <Card.Title></Card.Title>
+                <Card.Text>
+                  <span className="h3">{producto}</span>
+                  <hr />
+                  <Button variant="primary" className="linea button" size="sm" onClick={decrementar}>-</Button>
+                  <Form.Control type="text" className="linea input" size="sm" value={count} readOnly="readonly" min="0" />
+                  <Button variant="primary" className="linea button" size="sm" onClick={incrementar}>+</Button>
+                  <hr></hr>
+                  <div className="d-grid gap-2">
+                  <Button variant="primary" onClick={onAdd}>Agregar</Button>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default ItemCount;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Nav, Navbar, NavDropdown, Alert } from 'react-bootstrap';
 import { faHome, faAddressBook, faSoap, faSprayCan, faTooth, faMedkit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,9 @@ import CartWidget from './CartWidget';
 
 
 const NavBar = () => {
+
+  const [seccion, setSeccion] = useState('Inicio');
+
   return (<div>
     <Navbar bg="light" expand="lg">
   <Container>
@@ -17,23 +20,18 @@ const NavBar = () => {
       <Nav className="me-auto">
         <Nav.Link href="/"><FontAwesomeIcon icon={faHome} /> Inicio</Nav.Link>
         <NavDropdown title="Categorias" id="basic-nav-dropdown">
-          <NavDropdown.Item onClick={ () => SelectSeccion('Categoria > Desodorante') }><FontAwesomeIcon icon={faSprayCan} /> Desodorantes</NavDropdown.Item>
-          <NavDropdown.Item onClick={ () => SelectSeccion('Categoria > Jabones') }><FontAwesomeIcon icon={faSoap} /> Jabones</NavDropdown.Item>
-          <NavDropdown.Item onClick={ () => SelectSeccion('Categoria > Cuidado Dental') }><FontAwesomeIcon icon={faTooth} /> Cuidado Dental</NavDropdown.Item>
-          <NavDropdown.Item onClick={ () => SelectSeccion('Categoria > Labiales') }><FontAwesomeIcon icon={faMedkit} /> Botiquín</NavDropdown.Item>
+          <NavDropdown.Item onClick={ () => setSeccion('Categoria > Desodorante') }><FontAwesomeIcon icon={faSprayCan} /> Desodorantes</NavDropdown.Item>
+          <NavDropdown.Item onClick={ () => setSeccion('Categoria > Jabones') }><FontAwesomeIcon icon={faSoap} /> Jabones</NavDropdown.Item>
+          <NavDropdown.Item onClick={ () => setSeccion('Categoria > Cuidado Dental') }><FontAwesomeIcon icon={faTooth} /> Cuidado Dental</NavDropdown.Item>
+          <NavDropdown.Item onClick={ () => setSeccion('Categoria > Botiquín') }><FontAwesomeIcon icon={faMedkit} /> Botiquín</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link onClick={ () => SelectSeccion('Contáctenos') }><FontAwesomeIcon icon={faAddressBook} /> Contáctenos</Nav.Link>        
+        <Nav.Link onClick={ () => setSeccion('Contáctenos') }><FontAwesomeIcon icon={faAddressBook} /> Contáctenos</Nav.Link>        
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
-<Alert variant="info" id="info-seccion">Inicio</Alert>
+<Alert variant="info" id="info-seccion">{ seccion }</Alert>
   </div>);
 };
-
-const  SelectSeccion = (seccion) => {
-  document.getElementById('info-seccion').innerHTML = seccion;
-  console.log(seccion);
-}
 
 export default NavBar;
